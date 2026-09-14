bfe21bf8f11f076a2a4cb5506c90200cdcaa6dd3
OscarBendtsen <osbe4@elev.tec.dk>
2026-09-14 08:37:30 +0200

    Greyscale

---
+++ image_processing.py
@@ -0,0 +1,23 @@
+def IP(image):
+    import cv2
+
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("greyscale", img_gray)
+
+
+    cv2.waitKey(0) 
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    import os
+    import pathlib
+    import cv2
+    
+    mydir = os.path.expanduser("~\\OneDrive - TEC\\Informatik\\3.g\\Machine learning\\Projekt Nummerplade genkendelse\\Billeder_test")
+    
+    myfile = "IMG_7076.JPEG"
+    path = pathlib.Path(mydir, myfile)
+    image = cv2.imread(str(path))
+
+    IP(image)
+
