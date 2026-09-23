c65c75de2571d9cb305f755687608b62d6119158
OscarBendtsen <osbe4@elev.tec.dk>
2026-09-23 13:34:01 +0200

    idk

--- image_processing.py
+++ image_processing.py
@@ -1,8 +1,22 @@
 def IP(image):
     import cv2
 
+    
+
     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cv2.imshow("greyscale", img_gray)
+
+    thresh = cv2.threshold(img_gray, 1, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    cv2.imshow("threshold", thresh)
+
+    contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    i=0
+    for contour in contours:
+
+        if i==0:
+            i = 1
+            continue
 
 
     cv2.waitKey(0) 
@@ -18,6 +32,7 @@ if __name__ == "__main__":
     myfile = "IMG_7076.jpg"
     path = pathlib.Path(mydir, myfile)
     image = cv2.imread(str(path))
+    image = cv2.resize(image, (1000, 600))
 
     IP(image)
 
